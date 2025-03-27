7882fef999dd4cf476a5f526de3d9c2bd80f6f57
Andrujus <justasandrusk@gmail.com>
2025-03-27 12:57:43 +0200

    Sutvarkytas main failas

--- vektoriai.cpp
+++ vektoriai.cpp
@@ -1,12 +1,21 @@
 #include "functions.h"
 #include "student.h"
+#include <chrono>
+#include <iostream>
 
 int main() {
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    std::chrono::duration<double> diff;
     int ch1;
     std::vector<Student> studentai;
     std::vector<int> skaicius = {1000, 10000, 100000};
+
+    start = std::chrono::system_clock::now();
     Duom(studentai, ch1);
     Rez(studentai, ch1);
+    end = std::chrono::system_clock::now();  
     
+    diff = end - start;
+    std::cout << "Programos veikimo laikas: " << diff.count() << " s\n";
     return 0;
 }
